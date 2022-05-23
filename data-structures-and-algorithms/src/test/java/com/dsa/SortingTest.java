dbc7413bcfcac982508357909649ce17a534894c
MianAnswer <miananswer@hotmail.com>
2022-05-23 11:18:03 -0400

    Clean Up Tests

--- data-structures-and-algorithms/src/test/java/com/dsa/SortingTest.java
+++ data-structures-and-algorithms/src/test/java/com/dsa/SortingTest.java
@@ -9,12 +9,23 @@ import com.dsa.Sorting.Algorithms.*;
 import org.junit.*;
 
 public class SortingTest {
+    private static SortingAlgorithmFactory sortingAlgorithmFactory = new SortingAlgorithmFactory();
     private static final int[] arr = {
             220, 168, 415, 432, 367, 205, 425, 141, 225,
             32, 487, 398, 15, 397, 12, 1, 411, 301, 394,
             254, 48, 216, 218, 133, 21, 317, 200, 221, 46, 464
     };
     private static final int[] sortedCollectionArr = Arrays.copyOf(arr, arr.length);
+
+    private static int[] sortUsingAlgorithm(String sortingAlgorithmName) {
+        SortingAlgorithm sortingAlgorithm = sortingAlgorithmFactory.getSortingAlgorithm(sortingAlgorithmName);
+
+        int[] sortArr = Arrays.copyOf(arr, arr.length);
+
+        sortingAlgorithm.sort(sortArr);
+
+        return sortArr;
+    }
 
     @BeforeClass
     public static void init() {
@@ -23,34 +34,16 @@ public class SortingTest {
 
     @Test
     public void selectionSort() {
-        SortingAlgorithm sortingAlgorithm = new Selection();
-
-        int[] selectionArr = Arrays.copyOf(arr, arr.length);
-
-        sortingAlgorithm.sort(selectionArr);
-
-        assertArrayEquals(sortedCollectionArr, selectionArr);
+        assertArrayEquals(sortedCollectionArr, sortUsingAlgorithm("SELECTION"));
     }
 
     @Test
     public void insertionSort() {
-        SortingAlgorithm sortingAlgorithm = new Insertion();
-
-        int[] selectionArr = Arrays.copyOf(arr, arr.length);
-
-        sortingAlgorithm.sort(selectionArr);
-
-        assertArrayEquals(sortedCollectionArr, selectionArr);
+        assertArrayEquals(sortedCollectionArr, sortUsingAlgorithm("INSERTION"));
     }
 
     @Test
     public void bubbleSort() {
-        SortingAlgorithm sortingAlgorithm = new Bubble();
-
-        int[] selectionArr = Arrays.copyOf(arr, arr.length);
-
-        sortingAlgorithm.sort(selectionArr);
-
-        assertArrayEquals(sortedCollectionArr, selectionArr);
+        assertArrayEquals(sortedCollectionArr, sortUsingAlgorithm("BUBBLE"));
     }
 }

52c120ff7cb1ed24db4b2025e9ec241ff4dbc94c
MianAnswer <miananswer@hotmail.com>
2023-05-08 21:16:16 -0400

    Add height property to TreeNode

--- data-structures-and-algorithms/src/main/java/com/dsa/DataStructures/Helpers/TreeNode.java
+++ data-structures-and-algorithms/src/main/java/com/dsa/DataStructures/Helpers/TreeNode.java
@@ -4,21 +4,25 @@ public class TreeNode {
     public TreeNode left;
     public TreeNode right;
     public int val;
+    public int height;
 
     public TreeNode() {
         this.left = null;
         this.right = null;
+        this.height = 0;
     }
 
     public TreeNode(int val) {
         this.val = val;
         this.left = null;
         this.right = null;
+        this.height = 0;
     }
 
     public TreeNode(int val, TreeNode left, TreeNode right) {
         this.val = val;
         this.left = left;
         this.right = right;
+        this.height = Math.max(left.height, right.height);
     }
 }
